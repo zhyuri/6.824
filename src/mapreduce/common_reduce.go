b8a5e360d5b72a51f48b33ea98f281e94ef5ca9e
zhyuri <zhang1437@gmail.com>
2018-12-08 20:05:56 -0500

    [Lab 1: MapReduce] Part I: Map/Reduce input and output
    Test: go test -run Sequential

--- src/mapreduce/common_reduce.go
+++ src/mapreduce/common_reduce.go
@@ -1,5 +1,11 @@
 package mapreduce
 
+import (
+	"encoding/json"
+	"log"
+	"os"
+)
+
 func doReduce(
 	jobName string, // the name of the whole MapReduce job
 	reduceTask int, // which reduce task this is
@@ -17,14 +23,39 @@ func doReduce(
 	// reduceName(jobName, m, reduceTask) yields the file
 	// name from map task m.
 	//
-	// Your doMap() encoded the key/value pairs in the intermediate
-	// files, so you will need to decode them. If you used JSON, you can
-	// read and decode by creating a decoder and repeatedly calling
-	// .Decode(&kv) on it until it returns an error.
-	//
-	// You may find the first example in the golang sort package
-	// documentation useful.
-	//
+	keyValueMap := make(map[string][]string)
+	for m := 0; m < nMap; m++ {
+		interName := reduceName(jobName, m, reduceTask)
+		file, err := os.Open(interName)
+		if err != nil {
+			log.Fatalf("Cannot read file for reduce %v, err: %v", interName, err)
+		}
+		debug("Read intermediate file: %v\n", interName)
+		decoder := json.NewDecoder(file)
+
+		// Your doMap() encoded the key/value pairs in the intermediate
+		// files, so you will need to decode them. If you used JSON, you can
+		// read and decode by creating a decoder and repeatedly calling
+		// .Decode(&kv) on it until it returns an error.
+		//
+		// You may find the first example in the golang sort package
+		// documentation useful.
+		//
+		var kv KeyValue
+		for {
+			err := decoder.Decode(&kv)
+			if err != nil {
+				debug("Stop for loop with err: %v\n", err)
+				break
+			}
+			if values, ok := keyValueMap[kv.Key]; !ok {
+				keyValueMap[kv.Key] = []string{kv.Value}
+			} else {
+				keyValueMap[kv.Key] = append(values, kv.Value)
+			}
+		}
+	}
+
 	// reduceF() is the application's reduce function. You should
 	// call it once per distinct key, with a slice of all the values
 	// for that key. reduceF() returns the reduced value for that key.
@@ -41,7 +72,17 @@ func doReduce(
 	// 	enc.Encode(KeyValue{key, reduceF(...)})
 	// }
 	// file.Close()
-	//
-	// Your code here (Part I).
-	//
+	oFile, err := os.Create(outFile)
+	if err != nil {
+		log.Fatalf("Cannot create output file %v, err: %v", outFile, err)
+	}
+	defer oFile.Close()
+	enc := json.NewEncoder(oFile)
+
+	for key, values := range keyValueMap {
+		err := enc.Encode(KeyValue{key, reduceF(key, values)})
+		if err != nil {
+			log.Fatalf("Cannot encode key %v", key)
+		}
+	}
 }
