1e200c0af1bbe4f194e7bf8196ccceaefaae8329
zhyuri <zhang1437@gmail.com>
2018-12-08 20:22:21 -0500

    [Lab 1: MapReduce] Part II: Single-worker word count
    Test: ./test-wc.sh

--- src/main/wc.go
+++ src/main/wc.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"mapreduce"
 	"os"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 //
@@ -15,6 +18,14 @@ import (
 //
 func mapF(filename string, contents string) []mapreduce.KeyValue {
 	// Your code here (Part II).
+	words := strings.FieldsFunc(contents, func(r rune) bool {
+		return !unicode.IsLetter(r)
+	})
+	result := make([]mapreduce.KeyValue, 100)
+	for _, word := range words {
+		result = append(result, mapreduce.KeyValue{Key: word, Value: ""})
+	}
+	return result
 }
 
 //
@@ -24,6 +35,7 @@ func mapF(filename string, contents string) []mapreduce.KeyValue {
 //
 func reduceF(key string, values []string) string {
 	// Your code here (Part II).
+	return strconv.Itoa(len(values))
 }
 
 // Can be run in 3 ways:
